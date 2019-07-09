f4d7e43beeb40daa3d310e41fd2e6868219ceff5
alechp <me@alechp.com>
2019-07-09 14:45:13 -0700

    Help menu is passing. Went with condense-whitespace

--- test.js
+++ test.js
@@ -1,35 +1,25 @@
 const log = console.log;
 import test from "ava";
 import execa from "execa";
-import chalk from "chalk";
-import prettier from "prettier";
 import fs from "fs-extra";
+import condenseWhitspace from "condense-whitespace";
 import { helpMenuText } from "./cli";
 import { printLine, printMirror } from "tacker";
 
 test("main", async t => {
 	const { stdout } = await execa("./cli.js", ["--help"]);
 	const { description } = await fs.readJson("./package.json");
-	printLine("blue");
-	printMirror({ description }, "blue", "grey");
-	printLine("blue");
-	//TODO: Grab desc and prepend it to helpMenuText for it to match stdout
-	const pretty = {
+	const stripped = {
 		pStdout: (() => {
-			return prettier.format(stdout, { parser: "babel" });
+			return condenseWhitspace(stdout);
 		})(),
 		pHelp: (() => {
-			// prettier.format(`${description}\n${helpMenuText}`, { parser: "babel" });
-			let str = `${description}\n${helpMenuText}`;
-			return prettier.format(str, { parser: "babel" });
+			let str = `${description} ${helpMenuText}`;
+			return condenseWhitspace(str);
 		})()
 	};
-	const { pStdout, pHelp } = pretty;
-	// log(`pStdout: ${chalk.magenta(pStdout)}`);
-	// log(`pHelp: ${chalk.magenta(pHelp)}`);
-	printMirror({ pStdout }, "magenta", "grey");
-	printMirror({ pHelp }, "magenta", "grey");
-	// log(`${chalk.blue(stdout)}`);
-	// t.is(pStdout, pHelp);
-	t.pass();
+	const { pStdout, pHelp } = stripped;
+	// printMirror({ pStdout }, "magenta", "grey");
+	// printMirror({ pHelp }, "magenta", "grey");
+	t.is(pStdout, pHelp);
 });
