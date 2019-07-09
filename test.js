17294bc905c75cec0abfff069499df4b8092d42b
alechp <me@alechp.com>
2019-07-08 19:48:45 -0700

    Confirmed tacker working

--- test.js
+++ test.js
@@ -5,25 +5,30 @@ import chalk from "chalk";
 import prettier from "prettier";
 import fs from "fs-extra";
 import { helpMenuText } from "./cli";
+import { printLine, printMirror } from "tacker";
 
 test("main", async t => {
 	const { stdout } = await execa("./cli.js", ["--help"]);
 	const { description } = await fs.readJson("./package.json");
-	log(`Desc: ${description}`);
+	printLine("blue");
+	printMirror({ description }, "blue", "grey");
+	printLine("blue");
 	//TODO: Grab desc and prepend it to helpMenuText for it to match stdout
 	const pretty = {
 		pStdout: (() => {
-			prettier.format(stdout, { parser: "babel" });
+			return prettier.format(stdout, { parser: "babel" });
 		})(),
 		pHelp: (() => {
 			// prettier.format(`${description}\n${helpMenuText}`, { parser: "babel" });
 			let str = `${description}\n${helpMenuText}`;
-			prettier.format(str, { parser: "babel" });
+			return prettier.format(str, { parser: "babel" });
 		})()
 	};
 	const { pStdout, pHelp } = pretty;
-	log(`pStdout: ${chalk.magenta(pStdout)}`);
-	log(`pHelp: ${chalk.magenta(pHelp)}`);
+	// log(`pStdout: ${chalk.magenta(pStdout)}`);
+	// log(`pHelp: ${chalk.magenta(pHelp)}`);
+	printMirror({ pStdout }, "magenta", "grey");
+	printMirror({ pHelp }, "magenta", "grey");
 	// log(`${chalk.blue(stdout)}`);
 	// t.is(pStdout, pHelp);
 	t.pass();
